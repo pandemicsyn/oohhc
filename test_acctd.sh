2422b2f2b9980966e127310b1299ffc147684909
Jay Payne <jay.payne@rackspace.com>
2016-03-21 11:09:18 -0500

    Added a functional test file for oohhc-acctd

---
+++ test_acctd.sh
@@ -0,0 +1,90 @@
+#! /bin/bash
+# Functional test of oohhc-acctd
+
+# List accounts
+oohhc-cli -k 123456789 list | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Create account
+oohhc-cli -k 123456789 create -N company1 | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Get account
+oohhc-cli -k 123456789 get be1e82f6-652f-4840-97c6-aad31db6ff70 | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Update account (Name)
+oohhc-cli -k 123456789 update be1e82f6-652f-4840-97c6-aad31db6ff70 -N newname | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Update account (Token)
+oohhc-cli -k 123456789 update be1e82f6-652f-4840-97c6-aad31db6ff70 -T | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Update account (Status)
+oohhc-cli -k 123456789 update be1e82f6-652f-4840-97c6-aad31db6ff70 -S suspend | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Delete account
+oohhc-cli -k 123456789 be1e82f6-652f-4840-97c6-aad31db6ff70 | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
+
+# Update account (Undelete Status)
+oohhc-cli -k 123456789 update be1e82f6-652f-4840-97c6-aad31db6ff70 -S active | python -m json.tool
+if [ $? -eq 0 ]
+then
+  echo "========================================="
+  exit 0
+else
+  echo "*****************************************"
+  exit 1
+fi
